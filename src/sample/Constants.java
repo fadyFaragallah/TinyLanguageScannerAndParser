85bd2a727fb92938bbb6b5b5764c930f39571225
fadymedhat2236 <fady01288373385!new>
2018-11-04 11:28:27 +0200

    Enhancing the Scanner, Adding the enumerations and theConstants Class

---
+++ src/sample/Constants.java
@@ -0,0 +1,8 @@
+package sample;
+
+public class Constants {
+    public final static String IDENTIFIER="identifier";
+    public final static String NUMBER="number";
+    public final static String RESERVED_WORD="reserved word";
+    public final static String SPECIAL_SYMBOL="special symbol";
+}
