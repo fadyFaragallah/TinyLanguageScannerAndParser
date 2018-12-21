520aae332ef2d1f7ff86040fad0d45fe9cff1f60
fadyFaragallah <fadyfaragallah@gmail.com>
2018-12-21 19:14:44 +0200

    not sure if it's the final version or no but WHAT THE HELL not committing again

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,25 @@
 package sample;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.scene.web.PopupFeatures;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Callback;
 import org.json.JSONObject;
 
 import java.io.*;
@@ -66,7 +80,47 @@ public class Controller implements Initializable{
         //s.replace('[',' ');
 
         outputTextArea.setText(s);
+        try{
+            PrintStream out = new PrintStream(new FileOutputStream("output.js"));
+            out.print("var myObj="+s);
+        }catch(Exception e){
+
+        }
+
+        ///
 
 
+        StackPane secondaryLayout = new StackPane();
+        Scene secondScene = new Scene(secondaryLayout, 1000, 700);
+        WebView browser = new WebView();
+        WebEngine webEngine = browser.getEngine();
+        browser.sceneProperty().addListener(new ChangeListener<Scene>() {
+
+            @Override
+            public void changed(ObservableValue<? extends Scene> observable, Scene oldValue, Scene scene) {
+                if (scene != null) {
+                    browser.setMaxSize(secondScene.getWidth(),secondScene.getHeight());
+                    browser.maxWidthProperty().bind(secondScene.widthProperty());
+                    browser.maxHeightProperty().bind(secondScene.heightProperty());
+                } else {
+                    browser.maxWidthProperty().unbind();
+                    browser.maxHeightProperty().unbind();
+                }
+            }
+        });
+        File f = new File("index.html");
+        webEngine.load(f.toURI().toString());
+
+        secondaryLayout.getChildren().add(browser);
+
+
+
+        // New window (Stage)
+        Stage newWindow = new Stage();
+        newWindow.setTitle("Tiny Syntax Tree");
+        newWindow.setScene(secondScene);
+
+        newWindow.show();
+        ///
     }
 }
